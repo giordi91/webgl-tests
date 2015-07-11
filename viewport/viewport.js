04efabdad3484d195108b98c58c6a41754bdde11
Marco Giordano <marco.giordano.work@gmail.com>
2015-07-11 11:14:15 +0100

    fist basic working camera, still missing up rotation

--- viewport/viewport.js
+++ viewport/viewport.js
@@ -9,7 +9,11 @@ var MVP;
 var camPos;
 var lookAtPos;
 var program;
-
+var MOVEMENT_SPEED = 0.001;
+var ROTATION_SPEED = 0.1;
+var triangleB;
+var gridData = [];
+var gridB;
 
 window.onload = function init()
 {
@@ -27,37 +31,70 @@ window.onload = function init()
 
     
     //temp data
-    var vertices = [-16, -10,-50,
-                     0, 10,-50,
-                     10, -10, -50];
+    var vertices = [-5, -5,0,
+                     0, 5,0,
+                     5, -5, 0];
 
     //  Configure WebGL
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 0.6, 0.6, 0.6, 1.0 );
     
     
-    camPos = vec3(0,0,5); 
+    camPos = vec3(10,10,50); 
     lookAtPos = vec3(0,0,0);
     //  Load shaders and initialize attribute buffers
     program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
     
     // Load the data into the GPU
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    triangleB = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, triangleB);
     gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
 
     // Associate out shader variables with our data buffer
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
 
+
+    //grid data
+    for(i=-5; i<6; i++)
+    {
+       gridData.push(-5); 
+       gridData.push(0); 
+       gridData.push(i); 
+
+       gridData.push(5); 
+       gridData.push(0); 
+       gridData.push(i); 
+
+       gridData.push(i); 
+       gridData.push(0); 
+       gridData.push(-5); 
+
+       gridData.push(i); 
+       gridData.push(0); 
+       gridData.push(5); 
+    }
+    
+    for(i=-5; i<6; i++)
+    {
+    }
+    // Load the data into the GPU
+    gridB = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, gridB);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(gridData), gl.STATIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+    //var vPosition = gl.getAttribLocation( program, "vPosition" );
+    //gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+    gl.enable(gl.DEPTH_TEST);
     render();
 };
 
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
+
+    
+    
     //camera matrix
     projM = perspective(30, canvas.width/canvas.height, 0.1,100);
     var loc = gl.getUniformLocation(program, "projM");
@@ -71,8 +108,27 @@ function render() {
     MVP= mult(projM,ModelViewM);
     loc = gl.getUniformLocation(program, "MVP");
     gl.uniformMatrix4fv(loc,false,flatten(MVP));
-    
+
+    //draw triangle
+      
+    loc = gl.getUniformLocation(program, "color");
+    gl.uniform4fv(loc,flatten(vec4(1,0,0,1)));
+
+    gl.bindBuffer(gl.ARRAY_BUFFER,triangleB);
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
     gl.drawArrays( gl.TRIANGLES, 0, 3 );
+     
+    //draw grid 
+    loc = gl.getUniformLocation(program, "color");
+    gl.uniform4fv(loc,flatten(vec4(1,1,1,1)));
+    gl.bindBuffer(gl.ARRAY_BUFFER,gridB);
+    vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+    
+    gl.drawArrays( gl.LINES, 0, 44 );
     requestAnimFrame(render);
 }
 
@@ -86,6 +142,12 @@ function mouse_move_event(e)
         {
             camera_move(e.pageX, e.pageY);
         }
+        
+        if (e.button == 0)
+        {
+            camera_rotate(e.pageX, e.pageY);
+        }
+        
         //updating stored coordinate for computing delta
         old_x = e.pageX;
         old_y = e.pageY;
@@ -95,15 +157,15 @@ function mouse_move_event(e)
 function camera_move( x,y)
 {
     var aim = subtract(lookAtPos, camPos);
-    dx = old_x - x;
-    dy = old_y - y;
+    var dx = old_x - x;
+    var dy = old_y - y;
     var factor = length(aim);
 
     var dir = normalize(cross(aim,vec3(0,1,0)));
     var newup = normalize(cross(dir,aim));
 
     var offset = scale(factor,(add(scale(dx, dir ), scale(-dy, newup))));
-    offset = scale(0.006, offset);
+    offset = scale(MOVEMENT_SPEED, offset);
     if (length(offset) > 100)
     {
         return;
@@ -111,10 +173,25 @@ function camera_move( x,y)
     camPos = add(camPos,offset);
     lookAtPos = add(lookAtPos,offset);
     
-
-    
-
 }
-        
+       
+function camera_rotate(x,y)
+{
+    var dx = old_x - x;
+    var dy = old_y - y;
+    //removing pivot offset from camera and rotate from origin
+    var tempPos = subtract(camPos, lookAtPos);
+    
+    //y rot
+    var rotMat = rotate( dx*ROTATION_SPEED, vec3(0,1,0));
+    console.log(rotMat);
+    
+    var tempPosM = mat4();
+    tempPosM[3][0] = tempPos[0];
+    tempPosM[3][1] = tempPos[1];
+    tempPosM[3][2] = tempPos[2];
+    var res = mult(tempPosM,rotMat);
+    camPos = add(vec3(res[3][0],res[3][1],res[3][2]),lookAtPos);
+}
 
 
