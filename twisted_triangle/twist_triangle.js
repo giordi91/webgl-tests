c8dcd9131a4afa64a0e4a40cdda94ea11e44d9be
Marco Giordano <marco.giordano.work@gmail.com>
2015-07-12 16:11:15 +0100

    added extra stuff for hw

--- twisted_triangle/twist_triangle.js
+++ twisted_triangle/twist_triangle.js
@@ -1,37 +1,27 @@
 
 var gl;
 var points = [];
-
+var center;
+var recursion;
+var factor;  
+var vtx; 
+var program;
+var bufferId;
 window.onload = function init()
 {
     var canvas = document.getElementById( "gl-canvas" );
     
-    var RECURSION = 6;
-    var FACTOR = 1.5;
+    recursion= 0;
+    factor= 0.0;
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
-
+    document.getElementById("slider").onchange = function() { 
+            recursion= event.srcElement.value; eval_points();render();}
     
+    document.getElementById("tslider").onchange = function() { 
+            factor= event.srcElement.value; eval_points();render();}
     //generate the recursion data
-    var vtx = [-1, -1, 0, 1, 1, -1];
-    subdivide([vtx[0],vtx[1]],
-            [vtx[2],vtx[3]],
-            [vtx[4],vtx[5]], RECURSION);
-    
-    //rotate points
-    var center = [(vtx[0] + vtx[2] + vtx[4]) / 3.0,
-                (vtx[1] + vtx[3] + vtx[5]) / 3.0];
-    var dist;
-    var tmp;
-    for ( i=0; i< points.length; i++)
-    {
-        //points[i] = points[i] * cos(angle) 
-        dist = euclidian_diatance(points[i] , center);
-        tmp = rotation( points[i], FACTOR * dist);
-        points[i] = tmp;
-        //points[i+1] = tmp
-    }
-
+    vtx = [-0.6, -0.4, 0, 0.7, .6, -0.4];
     
     //  Configure WebGL
     gl.viewport( 0, 0, canvas.width, canvas.height );
@@ -39,12 +29,38 @@ window.onload = function init()
     
     //  Load shaders and initialize attribute buffers
     
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
+    eval_points();
     
+    render();
+};
+
+function eval_points()
+{
+
+    points =[];
+    subdivide([vtx[0],vtx[1]],
+            [vtx[2],vtx[3]],
+            [vtx[4],vtx[5]], recursion);
+    
+    //rotate points
+    //var center = [0,0];
+    center = [(vtx[0] + vtx[2] + vtx[4]) / 3.0,
+                (vtx[1] + vtx[3] + vtx[5]) / 3.0];
+    var dist;
+    var tmp;
+    for ( i=0; i< points.length; i++)
+    {
+        //points[i] = points[i] * cos(angle) 
+        dist = euclidian_diatance(points[i] , center);
+        tmp = rotation( points[i], factor* dist);
+        points[i] = tmp;
+        //points[i+1] = tmp
+    }
     // Load the data into the GPU
     
-    var bufferId = gl.createBuffer();
+    bufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
 
@@ -52,8 +68,7 @@ window.onload = function init()
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
-    render();
-};
+}
 
 function subdivide( v1, v2, v3, iter)
 {
@@ -92,10 +107,11 @@ function halfpoint ( v1,v2)
 
 function rotation( vec, angle)
 {
-    var res = [vec[0] * Math.cos(angle) - vec[1] * Math.sin(angle),
-            vec[0] * Math.sin(angle) + vec[1] * Math.cos(angle)    
+    var vec2 = [vec[0] - center[0], vec[1] -center[1]];
+    var res = [vec2[0] * Math.cos(angle) - vec2[1] * Math.sin(angle),
+            vec2[0] * Math.sin(angle) + vec2[1] * Math.cos(angle)    
         ];
-
+    res = [res[0] + center[0], res[1] + center[1]]; 
     return res;
 
 }
