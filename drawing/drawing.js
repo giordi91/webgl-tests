0ff70d285e183beb5a0eb87c29f6424f26c383a3
Marco Giordano <marco.giordano.work@gmail.com>
2015-07-22 21:45:53 +0100

    added color picker and first pass of width of the line using triangle strips

--- drawing/drawing.js
+++ drawing/drawing.js
@@ -5,42 +5,49 @@ var mouse_down= 0;
 var bufferId;
 var buffers_list=[];
 var buffers_sizes=[];
+var colors_list=[];
 var curr_buffer= null;
+var t_curr_buffer= null;
+var curr_color = [1,1,1,1];
 var points =[];
+var t_points =[];
 var canvas_w;
 var canvas_h;
 var program;
+var old = null;
+var LINE_WIDTH = 0.01;
 
 window.onload = function init()
 {
-    var canvas = document.getElementById( "gl-canvas" );
     canvas.oncontextmenu = function () {return false;};
     canvas.onmousedown= function() {mouse_down=true;};
     canvas.onmouseup = mouse_release_event;
     canvas.onmousemove = mouse_move_event;
+    canvas_w = canvas.width;
+    canvas_h = canvas.height;
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
     
-
-    canvas_w = canvas.width;
-    canvas_h = canvas.height;
+    //color picker initial confiugration 
+    $('#picker').colpick({
+        flat:true,
+        layout:'hex',
+        submit:0,
+        colorScheme:'dark',
+        onChange:set_color,
+        color:{r:255, g:255, b:255}});
     
-    var vertices = [-1, -1, 0, 1,1, -1,-1,-1];
 
     //  Configure WebGL
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( .1, .1, .1, 1.0 );
+    gl.clearColor( .1, .1, .1, 0.0 );
     
     //  Load shaders and initialize attribute buffers
     
     program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
     
-    // Load the data into the GPU
-    bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
 
     // Associate out shader variables with our data buffer
     console.log(remap_point(0,0));    
@@ -58,6 +65,8 @@ function render() {
         { 
             buffers_list[b].bind();
             var vPosition = gl.getAttribLocation( program, "vPosition" );
+            var loc = gl.getUniformLocation(program, "color");
+            gl.uniform4fv(loc,colors_list[b]);
             gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
             gl.enableVertexAttribArray( vPosition );
             gl.drawArrays(gl.LINE_STRIP,0, buffers_sizes[b]/2);
@@ -70,17 +79,24 @@ function render() {
         curr_buffer.bind();
         var vPosition = gl.getAttribLocation( program, "vPosition" );
         gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+        var loc = gl.getUniformLocation(program, "color");
+        gl.uniform4fv(loc,curr_color);
         gl.enableVertexAttribArray( vPosition );
         gl.drawArrays(gl.LINE_STRIP,0, points.length/2);
     }
-    gl.bindBuffer(gl.ARRAY_BUFFER,bufferId);
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
     
-    gl.drawArrays( gl.LINE_STRIP, 0, 4 );
-    
-    
+    if(t_curr_buffer && t_points.length>6)
+    {
+       t_curr_buffer.bind();
+        var vPosition = gl.getAttribLocation( program, "vPosition" );
+        gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+        var loc = gl.getUniformLocation(program, "color");
+        gl.uniform4fv(loc,curr_color);
+        gl.enableVertexAttribArray( vPosition );
+        //gl.drawArrays(gl.TRIANGLE_STRIP,0,t_points.length/2);
+        gl.drawArrays(gl.TRIANGLE_STRIP,0,t_points.length/2);
+    }
+
     requestAnimFrame(render);
 }
 
@@ -94,11 +110,74 @@ function mouse_move_event(e)
         {
             curr_buffer = new Buffer(gl,gl.ARRAY_BUFFER);
         } 
-        
+        if (!t_curr_buffer) 
+        {
+            t_curr_buffer = new Buffer(gl,gl.ARRAY_BUFFER);
+        } 
         var newp= remap_point(e.pageX-7,e.pageY-7);     
         points.push(newp[0],newp[1]);
         curr_buffer.bind(); 
         curr_buffer.upload(points, gl.DYNAMIC_DRAW);
+
+        //triangles
+        if (!old)
+        {
+            old = newp;
+        }
+        else
+        {
+            //extrating the delta
+            var delta = [newp[0] - old[0], newp[1] - old[1]];  
+            var delta3 = normalize(vec3(delta[0], delta[1],0.0));
+            var up = vec3(0,0,1); 
+            var perp3_v = scale(LINE_WIDTH,normalize(cross(delta3,up)));
+            //perp2_v = vec2(perp3
+            if (t_points.length<1)
+            {
+                console.log("base");
+                //compute the base of the strip 
+                var p1 = [old[0] + perp3_v[0],
+                          old[1] + perp3_v[1]];
+                
+                var p2 = [old[0] - perp3_v[0],
+                          old[1] - perp3_v[1]];
+                
+                var p3 = [newp[0] + perp3_v[0],
+                          newp[1] + perp3_v[1]];
+                
+                var p4 = [newp[0] - perp3_v[0],
+                          newp[1] - perp3_v[1]];
+            
+                t_points.push(p2[0],p2[1] ,
+                              p1[0],p1[1],
+                              p4[0],p4[1],
+                              p3[0],p3[1]); 
+            }
+            else
+            {
+                /*
+                if (t_points.length>=4)
+                {
+                
+                return;
+                }
+                */
+                var p1 = [newp[0] - perp3_v[0],
+                          newp[1] - perp3_v[1]];
+                
+                var p2 = [newp[0] + perp3_v[0],
+                          newp[1] + perp3_v[1]];
+                t_points.push(p1[0],p1[1] ,
+                              p2[0],p2[1])
+                console.log("regular");
+            } 
+
+            t_curr_buffer.bind();
+            t_curr_buffer.upload(t_points,gl.DYNAMIC_DRAW);
+        }
+
+            old = newp;
+
         
     }
 }
@@ -109,13 +188,14 @@ function mouse_release_event(e)
     mouse_down = false;
     if(points.length>4)
     {
-    buffers_list.push(curr_buffer);
-    buffers_sizes.push(points.length);
+        buffers_list.push(curr_buffer);
+        buffers_sizes.push(points.length);
+        colors_list.push(curr_color);
     }
 
     curr_buffer = null;
     points =[];
-
+    console.log("release"); 
 }
 
 function remap_point(x,y)
@@ -125,3 +205,9 @@ function remap_point(x,y)
     return [new_x, new_y];
 
 }
+
+function set_color(hsb,hex,rgb,el,bySetColor) 
+{
+        curr_color = [rgb.r/255,rgb.g/255,rgb.b/255,1.0];
+}
+
